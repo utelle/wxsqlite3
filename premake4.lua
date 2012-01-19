d36f39519bd53edcb934fe95b5a055d77dfedcb0
Ulrich Telle <utelle@users.noreply.github.com>
2012-01-19 20:11:06 +0000

    Regenerated configure files for Linux Fixed wxsqlite3def.h to eliminate gcc compiler warnings under Linux Added static wxSQLite3 library project to premake4.lua

--- premake4.lua
+++ premake4.lua
@@ -3,6 +3,55 @@ dofile "premake/wxwidgets.lua"
 solution "wxsqlite3"
   configurations { "Debug", "Release" }
 
+-- wxSQLite3 as static library
+project "wxsqlite3lib"
+  --uuid "8B4C3E27-674F-6740-B5F8-5E7BB48E1A9F"
+  kind "StaticLib"
+  language "C++"
+  vpaths {
+    ["Header Files"] = { "**.h" },
+    ["Source Files"] = { "**.cpp" }
+  }
+  files { "src/*.cpp", "include/wx/*.h" }
+  includedirs { "include", "sqlite3/include" }
+  flags { "Unicode" }  
+  links { "sqlite3" }
+  libdirs { "sqlite3/secure/aes128/dll/release" }
+
+  location "build/wxsqlite3lib"
+  targetname "wxsqlite3"
+
+  defines {
+    "WIN32",
+    "_WINDOWS",
+    "_LIB",
+    "WXMAKINGLIB_WXSQLITE3",
+    "wxUSE_DYNAMIC_SQLITE3_LOAD=0",
+    "WXSQLITE3_HAVE_METADATA=1",
+    "WXSQLITE3_HAVE_CODEC=1",
+    "WXSQLITE3_HAVE_LOAD_EXTENSION=0",
+    "_CRT_SECURE_NO_WARNINGS",
+    "_CRT_SECURE_NO_DEPRECATE",
+    "_CRT_NONSTDC_NO_DEPRECATE"
+  }
+
+  configuration "Debug"
+    targetdir "bin/lib/debug"
+    defines {
+      "DEBUG", 
+      "_DEBUG"
+    }
+    flags { "Symbols" }
+    wx_config {Unicode="yes", Version="2.8", Static="no", Debug="yes"}
+ 
+  configuration "Release"
+    targetdir "bin/lib/release"
+    defines { 
+      "NDEBUG" 
+    }
+    flags { "Optimize" }  
+    wx_config {Unicode="yes", Version="2.8", Static="no", Debug="no"}
+
 -- wxSQLite3 as shared library
 project "wxsqlite3dll"
   uuid "8B4C3E27-674F-6740-B5F8-5E7BB48E1A9F"
@@ -15,7 +64,7 @@ project "wxsqlite3dll"
   files { "src/*.cpp", "include/wx/*.h" }
   includedirs { "include", "sqlite3/include" }
   flags { "Unicode" }  
-  links { "libsqlite3" }
+  links { "sqlite3" }
   libdirs { "sqlite3/secure/aes128/dll/release" }
 
   location "build/wxsqlite3dll"
