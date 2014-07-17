a2335833461bab37241ecdb71db4f4faed925685
Ulrich Telle <utelle@users.noreply.github.com>
2014-07-17 06:15:13 +0000

    wxSQLite3: Fixed a bug in method operator= in class wxSQLite3StringCollection (recursive call to method operator=)

--- src/wxsqlite3.cpp
+++ src/wxsqlite3.cpp
@@ -4937,7 +4937,7 @@ wxSQLite3StringCollection::operator=(const wxSQLite3StringCollection& collection
 {
   if (this != &collection)
   {
-    wxSQLite3StringCollection::operator=(collection);
+    wxSQLite3NamedCollection::operator=(collection);
   }
   return *this;
 }
