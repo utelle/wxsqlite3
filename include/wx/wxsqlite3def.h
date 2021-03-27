1c06da0312cd60fbc0672e74d81673c02b7b1e5f
utelle <ulrich.telle@gmx.de>
2021-03-27 10:27:28 +0100

    Prepare release of wxSQLite3 4.6.8
    Upgrade to SQLite3 Multiple Ciphers version 1.2.3 (SQLite version 3.35.3)

--- include/wx/wxsqlite3def.h
+++ include/wx/wxsqlite3def.h
@@ -46,6 +46,11 @@
 
 <dl>
 
+<dt><b>4.6.8</b> - <i>March 2021</i></dt>
+<dd>
+Upgrade to <i>SQLite3 Multiple Ciphers version 1.2.3 (SQLite version 3.35.3)</i><br>
+
+</dd>
 <dt><b>4.6.7</b> - <i>March 2021</i></dt>
 <dd>
 Upgrade to <i>SQLite3 Multiple Ciphers version 1.2.2 (SQLite version 3.35.2)</i><br>
