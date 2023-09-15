d320de2bb76176620c81b14160b06977b0930fcb
utelle <ulrich.telle@gmx.de>
2023-09-15 21:57:06 +0200

    Prepare release of wxSQLite3 4.9.5
    - Update to SQLite3 Multiple Ciphers 1.6.5 (based on SQLite 3.43.1)

--- include/wx/wxsqlite3def.h
+++ include/wx/wxsqlite3def.h
@@ -46,6 +46,12 @@
 
 <dl>
 
+<dt><b>4.9.5</b> - <i>September 2023</i></dt>
+<dd>
+Upgrade to <i>SQLite3 Multiple Ciphers version 1.6.5 (SQLite version 3.43.1)</i><br>
+
+</dd>
+
 <dt><b>4.9.4</b> - <i>May 2023</i></dt>
 <dd>
 Upgrade to <i>SQLite3 Multiple Ciphers version 1.6.3 (SQLite version 3.42.0)</i><br>
