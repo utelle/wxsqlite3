e062687af114ac01d984f591704bb66cd80a90e5
utelle <ulrich.telle@gmx.de>
2025-05-05 18:21:19 +0200

    Prepare release of wxSQLite3 4.10.6

--- docs/html/search/enums_1.js
+++ docs/html/search/enums_1.js
@@ -3,10 +3,11 @@ var searchData=
   ['wxauthorizationcode_0',['wxAuthorizationCode',['../classwx_s_q_lite3_authorizer.html#adf245bc8ba906a2440d8eddd11d2e449',1,'wxSQLite3Authorizer']]],
   ['wxauthorizationresult_1',['wxAuthorizationResult',['../classwx_s_q_lite3_authorizer.html#adf7e1ec5f97c7c0d7cf8e2bcaadeb5d0',1,'wxSQLite3Authorizer']]],
   ['wxsqlite3ciphertype_2',['wxSQLite3CipherType',['../wxsqlite3_8h.html#aaf4137ac9a105d08d3bbf95f2a1785a8',1,'wxsqlite3.h']]],
-  ['wxsqlite3journalmode_3',['wxSQLite3JournalMode',['../wxsqlite3_8h.html#a52c53c308acf457f59ea3ead44f29fdf',1,'wxsqlite3.h']]],
-  ['wxsqlite3limittype_4',['wxSQLite3LimitType',['../wxsqlite3_8h.html#aec74080be17a473d33fa1a5933c236eb',1,'wxsqlite3.h']]],
-  ['wxsqlite3statementstatus_5',['wxSQLite3StatementStatus',['../wxsqlite3_8h.html#ae8ec729633cb94b85468fba5b91936ee',1,'wxsqlite3.h']]],
-  ['wxsqlite3transactionstate_6',['wxSQLite3TransactionState',['../wxsqlite3_8h.html#a7e374ef6aa338d31b05e4be5d5fe6661',1,'wxsqlite3.h']]],
-  ['wxsqlite3transactiontype_7',['wxSQLite3TransactionType',['../wxsqlite3_8h.html#af49cafcb237029e7e744a5b2076e2e5d',1,'wxsqlite3.h']]],
-  ['wxupdatetype_8',['wxUpdateType',['../classwx_s_q_lite3_hook.html#aaf1da5402c7ceb813ed216d237edc5cd',1,'wxSQLite3Hook']]]
+  ['wxsqlite3dbconfig_3',['wxSQLite3DbConfig',['../wxsqlite3_8h.html#ad52a48fbd0cc888a23adcd3033f463ba',1,'wxsqlite3.h']]],
+  ['wxsqlite3journalmode_4',['wxSQLite3JournalMode',['../wxsqlite3_8h.html#a52c53c308acf457f59ea3ead44f29fdf',1,'wxsqlite3.h']]],
+  ['wxsqlite3limittype_5',['wxSQLite3LimitType',['../wxsqlite3_8h.html#aec74080be17a473d33fa1a5933c236eb',1,'wxsqlite3.h']]],
+  ['wxsqlite3statementstatus_6',['wxSQLite3StatementStatus',['../wxsqlite3_8h.html#ae8ec729633cb94b85468fba5b91936ee',1,'wxsqlite3.h']]],
+  ['wxsqlite3transactionstate_7',['wxSQLite3TransactionState',['../wxsqlite3_8h.html#a7e374ef6aa338d31b05e4be5d5fe6661',1,'wxsqlite3.h']]],
+  ['wxsqlite3transactiontype_8',['wxSQLite3TransactionType',['../wxsqlite3_8h.html#af49cafcb237029e7e744a5b2076e2e5d',1,'wxsqlite3.h']]],
+  ['wxupdatetype_9',['wxUpdateType',['../classwx_s_q_lite3_hook.html#aaf1da5402c7ceb813ed216d237edc5cd',1,'wxSQLite3Hook']]]
 ];
